5539727f6ac49510194ffc24e0f0605bb62d6333
jappleyard28 <44617590+jappleyard28@users.noreply.github.com>
2023-11-29 20:15:07 +0000

    finished contact form

---
+++ src/components/ContactUs.js
@@ -0,0 +1,71 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+export const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_dmmmw5f', 'template_mdxw4nd', form.current, 'TAt3RSIkJ8t2B9Wv-')
+      .then((result) => {
+          console.log(result.text);
+          console.log("message successful")
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset(); // resets contact form fields to blank after submit button is pressed
+  };
+
+  return (
+    <div class="flex items-center justify-center p-12">
+        <div class="mx-auto w-full max-w-[550px]">
+            <form ref={form} onSubmit={sendEmail}>
+            <div class="mb-5">
+                <label for="name" class="mb-3 block text-base font-medium text-[#07074D]">Full Name</label>
+                <input type="text" name="from_name" id="name" placeholder="Full Name"
+                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+            </div>
+            <div class="mb-5">
+                <label for="email" class="mb-3 block text-base font-medium text-[#07074D]">Email Address</label>
+                <input
+                type="email"
+                name="from_email"
+                id="email"
+                placeholder="example@domain.com"
+                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                />
+            </div>
+            <div class="mb-5">
+                <label
+                for="subject"
+                class="mb-3 block text-base font-medium text-[#07074D]"
+                >
+                Subject
+                </label>
+                <input
+                type="text"
+                name="subject"
+                id="subject"
+                placeholder="Enter your subject"
+                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                />
+            </div>
+            <div class="mb-5">
+                <label for="message" class="mb-3 block text-base font-medium text-[#07074D]">Message</label>
+                <textarea
+                rows="4"
+                name="message"
+                id="message"
+                placeholder="Type your message"
+                class="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                ></textarea>
+            </div>
+            <div>
+                <input type='submit' value="Send" class="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-base font-semibold text-white outline-none" />
+            </div>
+            </form>
+        </div>
+    </div>
+  );
+};
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,12 @@
+import { ContactUs } from "../components/ContactUs";
+
 const Contact = () => {
     return (
         <div>
             <h1 class='flex justify-center font-sans text-5xl mb-10'>Contact Me</h1>
             <p class='flex justify-center'>Have a question or want to work together?</p>
             {/* contact me form */}
+            <ContactUs />
         </div>
     );
 };
