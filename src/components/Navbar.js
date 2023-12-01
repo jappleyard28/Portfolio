7ae7c74f46126d1756f5ee4fefc178dc5a14acd4
jappleyard28 <44617590+jappleyard28@users.noreply.github.com>
2023-12-01 05:17:30 +0000

    changed navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,22 +8,22 @@ function Navbar() {
         <nav class='flex flex-wrap text-xl p-5 text-gray-900 place-content-end'>
             <ul className='flex'>
                 <li>
-                    <Link to="/" class="home px-8 hover:text-gray-500">Home</Link>
+                    <Link to="/" class="px-8 hover:text-gray-400">Home</Link>
                 </li>
                 <li>
-                    <Link to="/about" class='px-8 hover:text-gray-500'>About</Link>
+                    <Link to="/about" class='px-8 hover:text-gray-400'>About</Link>
                 </li>
                 <li>
-                    <Link to="/education" class='px-8 hover:text-gray-500'>Education</Link>
+                    <Link to="/education" class='px-8 hover:text-gray-400'>Education</Link>
                 </li>
                 <li>
-                    <Link to="/projects" class='px-8 hover:text-gray-500'>Projects</Link>
+                    <Link to="/projects" class='px-8 hover:text-gray-400'>Projects</Link>
                 </li>
                 <li>
-                    <Link to="/contact" class='px-8 hover:text-gray-500'>Contact</Link>
+                    <Link to="/contact" class='px-8 hover:text-gray-400'>Contact</Link>
                 </li>
                 <li>
-                    <a href={resume} download="JackAppleyardCV" class='px-8 hover:text-gray-500'>CV</a>
+                    <a href={resume} download="JackAppleyardCV" class='px-8 hover:text-gray-400'>CV</a>
                 </li>
             </ul>
         </nav>
